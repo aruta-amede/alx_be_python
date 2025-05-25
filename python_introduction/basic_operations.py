3c8ea1a7488f92c5eb8ce838b083e62de748b562
Amede Aruta <arutashiran@gmail.com>
2025-05-25 22:56:52 +0300

    simple interest

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,9 +2,6 @@ number1 = 10
 number2 = 5
 
 sum = number1 + number2
-product = number1 * number2
 difference = number1 - number2
+product = number1 * number2
 
-print("sum:", sum)
-print("product:", product)
-print("difference:", difference)
